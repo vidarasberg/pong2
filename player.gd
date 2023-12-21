6934a9e94579774f1d2c5b47129c8e9d1fb808f6
Vidar Åsberg <vidar.asberg@protonmail.com>
2023-12-21 19:28:51 +0100

    adds controller rotation

--- player.gd
+++ player.gd
@@ -4,13 +4,15 @@ extends CharacterBody2D
 @export var move_left := "left"
 @export var move_down := "down"
 @export var move_up := "up"
+@export var rotate_right := "rotate_right"
+@export var rotate_left := "rotate_left"
 @export var color: Color
 
 var speed = 500
 var gravity = 0
 # This represents the player's inertia.
 var push_force = 80.0
-var rotation_speed = 2
+var rotation_speed = 0
 
 func _ready():
 	$ColorRect.set_color(color)
@@ -18,7 +20,8 @@ func _ready():
 func get_input():
 	var input_direction = Input.get_vector(move_left, move_right, move_up, move_down)
 	velocity = input_direction * speed
-	
+	rotation_speed = Input.get_axis(rotate_left, rotate_right) * 5
+
 
 func _physics_process(delta):
 	rotation += rotation_speed * delta
