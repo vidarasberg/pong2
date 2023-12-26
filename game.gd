3b1c2c8b024aafbaa47812108649a536692cbaff
Vidar Åsberg <vidar.asberg@protonmail.com>
2023-12-26 18:23:27 +0100

    adjusted physics based on feedback and automatic spin

--- game.gd
+++ game.gd
@@ -11,6 +11,8 @@ func _ready():
 	spawn_ball()
 	
 func spawn_ball():
+	var t = Timer.new() 		# Create a new Timer node
+	t.set_wait_time(5)
 	var ball = ball_scene.instantiate()
 	var viewportSize = get_viewport().get_visible_rect().size
 	ball.position = Vector2(viewportSize.x / 2, viewportSize.y / 2)
--- player.gd
+++ player.gd
@@ -8,11 +8,11 @@ extends CharacterBody2D
 @export var rotate_left := "rotate_left"
 @export var color: Color
 
-var speed = 500
+var speed = 600
 var gravity = 0
 # This represents the player's inertia.
-var push_force = 80.0
-var rotation_speed = 0
+var push_force = 100
+var rotation_speed = 5
 
 func _ready():
 	$ColorRect.set_color(color)
@@ -20,7 +20,7 @@ func _ready():
 func get_input():
 	var input_direction = Input.get_vector(move_left, move_right, move_up, move_down)
 	velocity = input_direction * speed
-	rotation_speed = Input.get_axis(rotate_left, rotate_right) * 5
+	#rotation_speed = Input.get_axis(rotate_left, rotate_right) * 5
 
 
 func _physics_process(delta):
